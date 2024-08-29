9b28ce69fb62e19d3e610c75cdb44e58ec6fb0ac
agodoy-digital-infosystem <agodoy@digital-infosystem.fr>
2024-08-29 10:21:31 +0200

    manage-orders: réactivation du filtre 'en cours' par défaut

--- sogapeint-CRM/sogapaint-crm/src/app/pages/manage-orders/manage-orders.component.ts
+++ sogapeint-CRM/sogapaint-crm/src/app/pages/manage-orders/manage-orders.component.ts
@@ -67,7 +67,7 @@ export class ManageOrdersComponent implements OnInit, OnDestroy {
     this.currentUser = this.userService.getCurrentUser();
     this.loadBenefits();
 
-    // this.activeTags.push("En cours"); // désactivation à la demande de Sogapeint
+    this.activeTags.push("En cours"); // désactivation à la demande de Sogapeint
 
     this.availableTags = this.availableTags.filter((tag) => tag !== "En cours");
 
